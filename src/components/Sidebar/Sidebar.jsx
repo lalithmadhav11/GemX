f2365fc4431ffa79208e8c10a5b06c63560bc03f
Anudeep Muppalla <anudeepmuppalla@gmail.com>
2025-10-25 10:24:45 +0530

    New Chat Bug Fix

--- src/components/Sidebar/Sidebar.jsx
+++ src/components/Sidebar/Sidebar.jsx
@@ -11,54 +11,60 @@ const Sidebar = () => {
 		await onSent(prompt);
 	};
 	return (
-		<div className="sidebar">
-			<div className="top">
-				<img
-					src={assets.menu_icon}
-					className="menu"
-					alt="menu-icon"
-					onClick={() => {
-						setExtended((prev) => !prev);
-					}}
-				/>
-				<div className="new-chat">
-					<img src={assets.plus_icon} alt="" onClick={()=>{
-                        newChat()
-                    }} />
-					{extended ? <p>New Chat</p> : null}
-				</div>
-				{extended ? (
-					<div className="recent">
-						<p className="recent-title">Recent</p>
-						{prevPrompts.map((item, index) => {
-							return (
-								<div onClick={()=>{
-                                    loadPreviousPrompt(item)
-                                }} className="recent-entry">
-									<img src={assets.message_icon} alt="" />
-									<p>{item.slice(0, 18)}...</p>
-								</div>
-							);
-						})}
-					</div>
-				) : null}
-			</div>
-			<div className="bottom">
-				<div className="bottom-item recent-entry">
-					<img src={assets.question_icon} alt="" />
-					{extended ? <p>Help desk</p> : null}
-				</div>
-				<div className="bottom-item recent-entry">
-					<img src={assets.history_icon} alt="" />
-					{extended ? <p>History</p> : null}
-				</div>
-				<div className="bottom-item recent-entry">
-					<img src={assets.setting_icon} alt="" />
-					{extended ? <p>Settings</p> : null}
-				</div>
-			</div>
-		</div>
-	);
+    <div className="sidebar">
+      <div className="top">
+        <img
+          src={assets.menu_icon}
+          className="menu"
+          alt="menu-icon"
+          onClick={() => {
+            setExtended((prev) => !prev);
+          }}
+        />
+        <div
+          className="new-chat"
+          onClick={() => {
+            newChat();
+          }}
+        >
+          <img src={assets.plus_icon} alt="" />
+          {extended ? <p>New Chat</p> : null}
+        </div>
+        {extended ? (
+          <div className="recent">
+            <p className="recent-title">Recent</p>
+            {prevPrompts.map((item, index) => {
+              return (
+                <div
+                  onClick={() => {
+                    loadPreviousPrompt(item);
+                  }}
+                  className="recent-entry"
+                >
+                  <img src={assets.message_icon} alt="" />
+                  <p>{item.slice(0, 18)}...</p>
+                </div>
+              );
+            })}
+          </div>
+        ) : null}
+      </div>
+      <div className="bottom">
+        <div className="bottom-item recent-entry">
+          <img src={assets.question_icon} alt="" />
+          {extended ? <p>Help desk</p> : null}
+        </div>
+        <div className="bottom-item recent-entry">
+          <img src={assets.history_icon} alt="" />
+          {extended ? <p>History</p> : null}
+        </div>
+        <div className="bottom-item recent-entry">
+          <img src={assets.setting_icon} alt="" />
+          {extended ? <p>Settings</p> : null}
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Sidebar;
